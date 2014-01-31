46334d2405430183c6231f907ca8a0f0d7b90ade
ewwink <ewwink@yahoo.com>
2014-01-31 22:06:20 +0700

    first

--- support/php.sh
+++ support/php.sh
@@ -12,14 +12,14 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-php_version="$1"
+PHP_VERSION="$1"
 mcrypt_version="2.5.8"
 
 if [ -z "$PHP_ZLIB_VERSION" ]; then
     PHP_ZLIB_VERSION=1.2.8
 fi
 
-echo "-----> Packaging PHP $php_version"
+echo "-----> Packaging PHP $PHP_VERSION"
 
 tempdir="$( mktemp -t php_xxxx )"
 rm -rf $tempdir
@@ -31,9 +31,9 @@ echo "-----> Downloading dependency zlib ${zlib_version}"
 curl -LO "http://zlib.net/zlib-${zlib_version}.tar.gz"
 tar -xzvf "zlib-${PHP_ZLIB_VERSION}.tar.gz"
 
-echo "-----> Downloading PHP $php_version"
-curl -LO "http://php.net/distributions/php-${php_version}.tar.gz"
-tar -xzvf "php-${php_version}.tar.gz"
+echo "-----> Downloading PHP $PHP_VERSION"
+curl -LO "http://php.net/distributions/php-${PHP_VERSION}.tar.gz"
+tar -xzvf "php-${PHP_VERSION}.tar.gz"
 
 mkdir -p "/app/vendor/php/zlib" "/app/vendor/libmcrypt" "/app/vendor/libicu"
 curl "http://${S3_BUCKET}.s3.amazonaws.com/package/libicu-51.tgz"
@@ -45,7 +45,7 @@ export PATH=/app/vendor/libicu/bin:\$PATH
 mkdir -p "/app/vendor/php/etc/conf.d"
 cd zlib-${PHP_ZLIB_VERSION} && 
 ./configure --prefix=/app/vendor/php/zlib && make && make install
-cd ../php-${php_version}
+cd ../php-${PHP_VERSION}
 
 ./configure --prefix=/app/vendor/php \
     --with-config-file-path=/app/vendor/php/etc \
@@ -82,11 +82,11 @@ make && make install
 echo "-----> Uploading source to build server"
 
 cd /app/vendor/php
-tar -cvzf $tempdir/php-${php_version}.tgz .
+tar -cvzf $tempdir/php-${PHP_VERSION}.tgz .
 
-"$basedir/checksum.sh" "$tempdir/php-${php_version}.tgz"
+"$basedir/checksum.sh" "$tempdir/php-${PHP_VERSION}.tgz"
 
-echo "-----> Done building PHP package! saved as $tempdir/php-${php_version}.tgz"
+echo "-----> Done building PHP package! saved as $tempdir/php-${PHP_VERSION}.tgz"
 
 echo "-----------------------------------------------"
 
@@ -94,7 +94,7 @@ echo "---> Uploading package to FTP Server"
 while true; do
     read -p "Do you wish to to Upload to FTP Server (y/n)?" yn
     case ${yn} in
-        [Yy]* ) "$basedir/ftp-upload.sh" "$tempdir/php-${php_version}.tgz"; break;;
+        [Yy]* ) "$basedir/ftp-upload.sh" "$tempdir/php-${PHP_VERSION}.tgz"; break;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
     esac
