6080257ea562ac5cc6f0df8a2de2f84b4f8b315d
ewwink <ewwink@yahoo.com>
2014-01-31 21:55:39 +0700

    first

---
+++ support/set-env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export FTP_HOST="lyric.net23.net"
+export FTP_DIR="/public_html/nginx-php-apc/"
+
+export LIBMCRYPT_VERSION="2.5.8"
+export LIFREETYPE_VERSION="2.4.12"
+export PHP_VERSION="5.5.8"
+export PHPREDIS_VERSION="2.2.2"
+export NEWRELIC_VERSION="4.4.5.35"
+
+export NGINX_VERSION="1.5.9"
+
